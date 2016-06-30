4b64fcab43d5a350d9cb54eec9b6def946ac3376
Albana <albana@prova.it>
2016-06-30 14:54:12 +0200

    primo commit di questo progetto

---
+++ src/EsercizioMio.java
@@ -0,0 +1,13 @@
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
+
+import javax.swing.*;
+
+public class EsercizioMio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
